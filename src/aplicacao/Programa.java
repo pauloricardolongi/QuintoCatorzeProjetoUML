7d72759410ecfed0271eee1e6dc27ebb3c24db3b
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-05 23:04:18 -0300

    melhorando exp. lambda

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -16,10 +16,7 @@ List<Produto>list = new ArrayList<>();
 		list.add(new Produto("Notebook", 1200.00));
 		list.add(new Produto("Tablete",450.00));	
 		
-		
-		Comparator<Produto> comp = (p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
-		
-		list.sort(comp );
+		list.sort((p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase()));
 		
 		for(Produto p : list) {
 			System.out.println(p);
