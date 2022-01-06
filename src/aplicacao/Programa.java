7e0c7121adb6faea4918b57cccb8b98cb6b4919e
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-05 22:55:52 -0300

    melhorar exp. lambda

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -14,14 +14,10 @@ List<Produto>list = new ArrayList<>();
 		
 		list.add(new Produto("Tv",900.00));
 		list.add(new Produto("Notebook", 1200.00));
-		list.add(new Produto("Tablete",450.00));
+		list.add(new Produto("Tablete",450.00));	
 		
-		//classe anônima dentro programa principal
 		
-		Comparator<Produto> comp = (p1, p2) -> {
-			return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
-			
-		};
+		Comparator<Produto> comp = (p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
 		
 		list.sort(comp );
 		
